fe251908c1715b65aa667276e8e7d682ea25533c
Craig Condon <craig.condon@hellosign.com>
2015-05-01 14:42:07 -0700

    cleanup

--- README.md
+++ README.md
@@ -1,29 +1,25 @@
 basic RPC system for mesh. Allows you to execute remote operations, and receive responses.
 
+Bus example:
+
 ```javascript
-var ros = require("ros");
-var mesh = require("mesh");
-var io   = require("socket.io");
+var ros    = require("ros");
+var mesh   = require("mesh");
+var io     = require("socket.io");
+var memory = require("mesh-memory");
 
-var client = io("http://127.0.0.1");
 
-var localBus = mesh.wrap(function(operation, next) {
-  next();
+
+function realtime(options, appBus) {
+  var client = io(options);
+  return ros(client.on.bind(client, "message"), client.emit.bind("message"), appBus);
+}
+
+// local
+var bus = realtime("http://localhost:8080");
+
+bus(mesh.op("load")).on("data", function() {
+
 });
 
-var bus = ros(function(handler) {
-
-  // listen on the client for remote operations
-  client.on("message", handler.intercept(localBus));
-
-  // handles operations executed locally
-  return function(operation) {
-    client.emit("message", operation);
-  }
-});
-
-
-bus(mesh.op("insert", { data: { name: "blarg" }})).on("data", function() {
-  // do stuff
-});
 ```
--- test/basic-test.js
+++ test/basic-test.js
@@ -6,33 +6,16 @@ var EventEmitter = require("events").EventEmitter;
 describe(__filename + "#", function() {
 
   it("can create a bus", function() {
-    ros(function() {});
+    ros(mesh.noop, function() { }, mesh.noop);
   });
 
-  it("tags operations as they're going through the bus", function(next) {
-
-
-    var b2 = ros(function() {
-      return function(op) {
-        expect(op.req).to.be(1);
-        next();
-      }
-    });
-
-    b2(mesh.op("a"));
-  });
 
   it("can return data from a remote source", function(next) {
     var em = new EventEmitter();
-    var bus = ros(function(handler) {
-      em.on("message", handler.intercept(mesh.stream(function(operation, stream) {
-        stream.end("data");
-      })));
 
-      return function(op) {
-        em.emit("message", op);
-      };
-    });
+    var bus = ros(em.on.bind(em, "message"), em.emit.bind(em, "message"), mesh.stream(function(operation, stream) {
+      stream.end("data");
+    }));
 
     var data;
 
@@ -46,16 +29,10 @@ describe(__filename + "#", function() {
 
   it("properly sends back an error", function(next) {
     var em = new EventEmitter();
-    var bus = ros(function(handler) {
 
-      em.on("message", handler.intercept(mesh.wrap(function(operation, next) {
-        next(new Error("error!"));
-      })));
-
-      return function(op) {
-        em.emit("message", op);
-      }
-    });
+    var bus = ros(em.on.bind(em, "message"), em.emit.bind(em, "message"), mesh.wrap(function(operation, next) {
+      next(new Error("error!"));
+    }));
 
 
     bus(mesh.op("load")).on("error", function(err) {
@@ -64,33 +41,13 @@ describe(__filename + "#", function() {
     });
   });
 
-  it("can have an 'else' handler for the intercept function", function(next) {
-    var em = new EventEmitter();
-
-    ros(function(handler) {
-      em.on("message", handler.intercept(mesh.noop, function(message) {
-        expect(message.text).to.be("abba");
-        next();
-      }));
-
-      em.emit("message", { text: "abba" });
-    });
-  });
 
   it("ignores remote operations from being re-used", function(next) {
     var em = new EventEmitter();
-    var bus = ros(function(handler) {
 
-      // should end up with infinite loop
-      em.on("message", handler.intercept(mesh.stream(function(operation, stream) {
-        bus(operation).pipe(stream);
-      })));
-
-      return function(operation) {
-        em.emit("message", operation);
-      }
-    });
-
+    var bus = ros(em.on.bind(em, "message"), em.emit.bind(em, "message"), mesh.stream(function(operation, stream) {
+      bus(operation).pipe(stream);
+    }));
 
     bus(mesh.op("load")).on("end", function() {
       next();
@@ -100,14 +57,10 @@ describe(__filename + "#", function() {
   it("can flag an operation to not look for a response", function(next) {
     var em = new EventEmitter();
 
-    var bus = ros(function(handler) {
-      em.on("message", handler.intercept(mesh.wrap(function(operation, next) {
-        next(void 0, "abba");
-      })))
-      return function(operation) {
-        em.emit("message", operation);
-      };
-    });
+    var bus = ros(em.on.bind(em, "message"), em.emit.bind(em, "message"), mesh.wrap(function(operation, next) {
+      next(void 0, "abba");
+    }));
+
 
     var _i = 0;
 
